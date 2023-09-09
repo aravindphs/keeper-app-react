981475fa66c509c57c043889d3da16e09db2ddbf
Aravind Kumar J <aravind94977034@gmail.com>
2023-09-09 10:49:48 +0530

    everything done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
 
 const Header = ()=>{
     return <div>
         <header>
-            <h1>Bliss Vista</h1>
+            <h1><TipsAndUpdatesIcon /> Bliss Vista</h1>
         </header>
     </div>
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const Note = (props)=>{
     const handleClick = () => {
@@ -7,7 +8,7 @@ const Note = (props)=>{
     return <div className="note">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
-        <button onClick={handleClick}>DELETE</button>
+        <button onClick={handleClick}><DeleteIcon /></button>
     </div>
 }
 
